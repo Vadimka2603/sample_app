73bbe5849c69bbc9e06f1404423928453615af5f
Timenaser <vad-nes@yandex.ru>
2015-03-21 16:53:31 +0400

    Глава 7. Регистрация

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
+  
   def new
+  	@user = User.new						#Adding an @user variable to the new action.
   end
+
+  def show
+  @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)    
+    if @user.save
+    	flash[:success] = "Welcome to the Sample App!"
+    	redirect_to @user
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
+
