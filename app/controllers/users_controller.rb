b809045b36545bb3eed0c91aa519175d74f9a6ea
Timenaser <vad-nes@yandex.ru>
2015-03-24 01:47:31 +0400

    Finish user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -41,14 +42,13 @@ class UsersController < ApplicationController
     end
   end
 
-  private
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
-
-    def logged_in_user
+  def logged_in_user
       unless logged_in?
         store_location
         flash[:danger] = "Please log in."
@@ -56,9 +56,20 @@ class UsersController < ApplicationController
       end
     end
 
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+      def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar")
+User.create!(name:  "vad",
+             email: "vad-nes@yandex.ru",
+             password:              "0112984",
+             password_confirmation: "0112984",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
